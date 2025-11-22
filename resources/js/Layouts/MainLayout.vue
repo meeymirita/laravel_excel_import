e435bc4c9d676bd4c61e6606dcf3a51086e00dfa
nikita <popap182@gmail.com>
2025-11-22 21:26:09 +0300

    inst: inertia, vue, laravel breeze, new layout for excel import

---
+++ resources/js/Layouts/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+<div class="w-96 mx-auto">
+    <h1>my app</h1>
+    <div
+        class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex"
+    >
+        <Link
+            :href="route('dashboard')"
+            :active="route().current('dashboard')"
+        >
+            Dashboard
+        </Link>
+
+        <Link
+            :href="route('project.index')"
+            :active="route().current('project.index')"
+        >
+            Project
+        </Link>
+        <Link
+            :href="route('project.import')"
+            :active="route().current('project.import')"
+        >
+            Import
+        </Link>
+        <Link
+            :href="route('task.index')"
+            :active="route().current('task.index')"
+        >
+            Task
+        </Link>
+    </div>
+    <slot></slot>
+</div>
+</template>
+<script>
+import {Link} from "@inertiajs/vue3";
+
+export default {
+    name: "MainLayout",
+    components: {Link},
+}
+</script>
+<style scoped>
+
+</style>
---
+++ resources/js/Pages/Project/Index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="w-96 mx-auto">
+        Index
+    </div>
+</template>
+<script>
+import MainLayout from "@/Layouts/MainLayout.vue";
+
+export default {
+    name: "Index",
+    layout: MainLayout
+}
+</script>
+<style scoped>
+
+</style>
